792222809c24e6c0fbe49aeff59e7bfc245df692
Tiffany <rosalie568@hotmail.com>
2016-08-30 16:46:28 -0500

    Added newpost.html page

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
+# Shows index page to link to main blog page
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
@@ -13,6 +14,7 @@ class MainHandler(webapp2.RequestHandler):
         response = t.render()
         self.response.write(response)
 
+# Shows most recent 5 posts on BLOG
 class BlogPage(webapp2.RequestHandler):
 
     def get(self):
@@ -20,7 +22,19 @@ class BlogPage(webapp2.RequestHandler):
         response = t.render()
         self.response.write(response)
 
+# Shows New Post page
+class NewPost(webapp2.RequestHandler):
+
+    def get(self):
+        title = self.request.get("title")
+        blog = self.request.get("blog")
+
+        t = jinja_env.get_template("newpost.html")
+        response = t.render(title ="", blog="")
+        self.response.write(response)
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/blog', BlogPage)
+    ('/blog', BlogPage),
+    ('/newpost', NewPost)
 ], debug=True)
