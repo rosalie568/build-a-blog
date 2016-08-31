a00906d406ada55c5ae335a258cda6633e2fbbe8
Tiffany <rosalie568@hotmail.com>
2016-08-31 13:04:15 -0500

    Some work done on database

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import webapp2
 import jinja2
 import os
 
+#for database
+from google.appengine.ext import db
+
 #set up jinja
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
+#for database handling
+class Posts(db.Model):
+    title = db.StringProperty(required=True)
+    blog = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
 # Shows index page to link to main blog page
 class MainHandler(webapp2.RequestHandler):
-
     def get(self):
         t = jinja_env.get_template("index.html")
         response = t.render()
@@ -16,15 +24,14 @@ class MainHandler(webapp2.RequestHandler):
 
 # Shows most recent 5 posts on BLOG
 class BlogPage(webapp2.RequestHandler):
-
     def get(self):
+        post = db.GqlQuery("Select * from Posts order by created DESC Limit 5")
         t = jinja_env.get_template("mainblog.html")
-        response = t.render()
+        response = t.render( post )
         self.response.write(response)
 
 # Shows New Post page
 class NewPost(webapp2.RequestHandler):
-
     def get(self):
         t = jinja_env.get_template("newpost.html")
         response = t.render(title ="", err_title="", blog="", err_blog="")
@@ -46,8 +53,10 @@ class NewPost(webapp2.RequestHandler):
             blog_bool = False
             err_blog = "Need value for blog!"
 
-        #if both values are not empty redirect to mainblog page
+        #if both values are not empty redirect to mainblog page & store values in database
         if title_bool and blog_bool:
+            post = Posts(title=title, blog=blog)
+            post.put()
             t = jinja_env.get_template("mainblog.html")
             response = t.render()
             self.response.write(response)
@@ -59,5 +68,5 @@ class NewPost(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/blog', BlogPage),
-    ('/newpost', NewPost)
+    ('/blog/newpost', NewPost)
 ], debug=True)
